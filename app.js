20976b52dc8f84e006cab1a1b38b785c0502c0fa
Yuri <snicksnk@MacBook-Pro-Yuri.local>
2018-06-06 17:13:20 +0400

    Dumb adding of elements

--- app.js
+++ app.js
@@ -14,3 +14,25 @@ const svg = d3.select('body').append('svg')
     .attr('height', height + margin.top + margin.bottom)
   .append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+
+const data = [
+  { id: 1, title: 'hello', value: 22 },
+  { id: 2, title: 'hello1', value: 2 },
+  { id: 3, title: 'hello2', value: 122 },
+  { id: 4, title: 'hello3', value: 322 },
+];
+
+function draw() {
+  data.map((item, n) => {
+    // https://github.com/d3/d3-selection
+    svg.append('rect')
+      .attr('class', 'bar')
+      .attr('width', item.value)
+      .attr('y', n * 100)
+      .attr('height', 100);
+  });
+}
+
+
+draw();
